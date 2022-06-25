f91b433e321b790dca0f721ba2f753e10169c2a3
ymgood <minala7870@gmail.com>
2022-06-25 20:32:57 +0900

    first git

--- app/src/main/java/com/goMars/yeongsungbus/screen/MainActivity.kt
+++ app/src/main/java/com/goMars/yeongsungbus/screen/MainActivity.kt
@@ -1,26 +1,22 @@
 package com.goMars.yeongsungbus.screen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import com.goMars.yeongsungbus.R
+import androidx.appcompat.app.AppCompatActivity
 import com.goMars.yeongsungbus.databinding.ActivityMainBinding
-import com.goMars.yeongsungbus.methods.screenSize.ScreenSize
 
 class MainActivity : AppCompatActivity() {
     val Tag: String = "MainActivity : "
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnNext.setOnClickListener(btnListener)
+
 
     }
 
