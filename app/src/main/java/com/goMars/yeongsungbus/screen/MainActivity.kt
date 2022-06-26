a318d1ad7bdf1c63808e740b3586c747154f1e94
ymgood <minala7870@gmail.com>
2022-06-26 20:25:34 +0900

    main page ui 수정

--- app/src/main/java/com/goMars/yeongsungbus/screen/MainActivity.kt
+++ app/src/main/java/com/goMars/yeongsungbus/screen/MainActivity.kt
@@ -18,34 +18,16 @@ import com.goMars.yeongsungbus.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     val Tag: String = "MainActivityTest"
     private lateinit var binding: ActivityMainBinding
-    var position_flag = true
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        supportActionBar?.setTitle("                      연성대 스쿨버스")
-//        supportActionBar?.setSubtitle("                      안양           시외          하나")
         setContentView(binding.root)
         binding.collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE)
 
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle presses on the action bar items
-        when(item.itemId){
-            R.id.action_btn1 -> { return false }
-            R.id.action_btn2 -> { return false }
-            else -> {return super.onOptionsItemSelected(item)}
-
-
-
-        }
-    }
 
     val btnListener : View.OnClickListener = View.OnClickListener {
         startActivity(Intent(this,LongDistanceBusActivity::class.java))
